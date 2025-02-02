ef066ea3a1e2fb62c482b90faa5a56c37b8aafec
dinesh1816 <ddinesh1816@gmail.com>
2025-02-02 15:55:15 -0600

    contact me to contact changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
             <li><Link to="skills" smooth={true}>Skills</Link></li>
             <li><Link to="projects" smooth={true}>Projects</Link></li>
             <li><Link to="experience" smooth={true}>Experience</Link></li>
-            <li><Link to="contact" smooth={true}>Contact Me</Link></li>
+            <li><Link to="contact" smooth={true}>Contact</Link></li>
           </ul>
         </nav>
       </div>
